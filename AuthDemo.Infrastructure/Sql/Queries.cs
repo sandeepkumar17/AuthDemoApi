922f2b23ba2eabe81b92a12aaf029367036c9677
Sandeep <TEI1126@KINETICTEST>
2024-06-17 10:53:29 +0530

    Updated the API to .NET 8.0

--- AuthDemo.Infrastructure/Sql/Queries.cs
+++ AuthDemo.Infrastructure/Sql/Queries.cs
@@ -2,11 +2,14 @@
 {
     public static class Queries
     {
+		//Get All Users
 		public static string AllUsers => "SELECT [Id], [FirstName], [LastName], [UserName] FROM [dbo].[User]";
 
-		public static string UserById => "SELECT [Id], [FirstName], [LastName], [UserName] FROM [dbo].[User] WHERE [Id] = @Id";
+        //Get User By Id
+        public static string UserById => "SELECT [Id], [FirstName], [LastName], [UserName] FROM [dbo].[User] WHERE [Id] = @Id";
 
-		public static string AuthenticateUser =>
+        //Authenticate User
+        public static string AuthenticateUser =>
 			@"SELECT [Id], [FirstName], [LastName], [UserName]
 			FROM [dbo].[User]
 			WHERE [UserName] = @UserName
